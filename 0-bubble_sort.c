62bc3db857ff00f58781f9df55ed8003009c6b32
esteban-silvera <estebansilvera000@gmail.com>
2023-09-08 14:24:55 +0000

    fix bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
-#include "main.c"
+#include "sort.h"
 
 void
 bubble_sort(int *array, size_t size)
 {
-
+	int i, j, temp;
+	for (i = size -1 ; i >= 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] > array[j +1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
