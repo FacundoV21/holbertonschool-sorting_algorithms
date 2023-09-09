d2ea12732d979a3a403a16856a4ce4c320af509f
esteban-silvera <estebansilvera000@gmail.com>
2023-09-09 13:08:18 -0700

    lis array

--- print_list.c
+++ print_list.c
@@ -2,33 +2,26 @@
 #include <stdlib.h>
 #include "sort.h"
 
-/**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
-listint_t *create_listint(const int *array, size_t size)
-{
-	listint_t *list;
-	listint_t *node;
-	int *tmp;
+#include <stdio.h>
+#include "sort.h"
 
-	list = NULL;
-	while (size--)
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+	int i;
+
+	i = 0;
+	while (list)
 	{
-		node = malloc(sizeof(*node));
-		if (!node)
-			return (NULL);
-		tmp = (int *)&node->n;
-		*tmp = array[size];
-		node->next = list;
-		node->prev = NULL;
-		list = node;
-		if (list->next)
-			list->next->prev = list;
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
 	}
-	return (list);
+	printf("\n");
 }
